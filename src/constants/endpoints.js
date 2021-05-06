1612da5df409317ec7268b6e572ca0713669ba0e
SergioRico1 <51226753+SergioRico1@users.noreply.github.com>
2021-05-06 17:20:41 +0200

    Update endpoints.js

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,6 @@
 const endpoints = {
   gateway: 'https://dweb.link/ipfs',
-  api: 'https://ipfs.infura.io:5001/api/v0',
+  api: 'https://ipfs.infura.io:5001/api',
   share: 'https://ipfscloud.netlify.app/#',
   apiMultiAddr: '/dns4/ipfs.io/tcp/443/https'
 }
